abea2561ce8353161b699123a46ca62a2f016868
PATELH78 <155693665+PATELH78@users.noreply.github.com>
2024-03-19 18:53:07 -0600

    Add files via upload

---
+++ patelh78_Assignment2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int EvenNum = 0;
+        int OddNum = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"{i} is even number.");
+                EvenNum += i;
+            }
+            else
+            {
+                Console.WriteLine($"{i} is odd number.");
+                OddNum  += i;
+            }
+        }
+
+        Console.WriteLine($"Total of even numbers: {EvenNum}");
+        Console.WriteLine($"Total of odd numbers: {OddNum}");
+    }
+}
